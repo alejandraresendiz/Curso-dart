07b2c4c43677445d3a171d09ce31326af09d8a01
alejandraresendiz <lizethgarcia5272@gmail.com>
2025-04-02 08:55:05 -0600

    Revert "Create operadores_ejercicio1.dart"
    This reverts commit 6fa373390f4dbad2f1aca7ec727ef8686943b51c.

--- bin/operadores_ejercicio1.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io' ;
-
-void main (List<String> args){
-
-  stdout.write('Ingrese la temperatura en grados centígrados:');
-  int num1= int.parse(stdin.readLineSync()!);
-
- print('La temperatura en Fahrenheit es: ${num1* 9/5 + 32} ') ;
-}
